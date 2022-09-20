46c6599c1638948e23041f3abfce24e689f1e220
Lyov97 <69639055+Lyov97@users.noreply.github.com>
2022-09-20 10:58:18 +0400

    Create day10_2.py

---
+++ day10_2.py
@@ -0,0 +1,22 @@
+#Python Day 10, Assessment 2
+
+'''Question: 
+Напишите программу, которая выводит чётные числа из заданного списка и
+останавливается, если встречает число 616.
+'''
+
+def black_number():
+
+    num = input().split(' ')
+    
+    for i in num:
+    
+       if int(i) == 616:
+    
+           break;
+    
+       elif int(i)%2 == 0:
+    
+           print(i)
+           
+black_number()           
